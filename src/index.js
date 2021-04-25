15ee8f4d58b1d4bb2bb976b1851e4f50bd4897f9
ric-cde <39746198+ric-cde@users.noreply.github.com>
2021-04-26 00:18:00 +0100

    hotfix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-
+import presentation from './static/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf';
+import report from './static/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf';
 
 
 class App extends React.Component {
@@ -17,11 +18,11 @@ class App extends React.Component {
     return (
     <div class="container">
       <div class="child">
-      <p><a href={process.env.PUBLIC_URL + "/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf"}>Part 1: Presentation</a><br /><br /></p>
-      <p><a href={process.env.PUBLIC_URL + "/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf"}>Part 2-4: Reports</a></p><br />
-      <iframe src={process.env.PUBLIC_URL + "/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf"} width="1000px" height="800px"></iframe>
+      <p><a href={presentation}>Part 1: Presentation</a><br /><br /></p>
+      <p><a href={report}>Part 2-4: Reports</a></p><br />
+      <iframe src={presentation} width="1000px" height="800px"></iframe>
       <br /><br />
-      <iframe src={process.env.PUBLIC_URL + "/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf"} width="1000px" height="800px"></iframe>
+      <iframe src={report} width="1000px" height="800px"></iframe>
       </div>
     </div>
     )
