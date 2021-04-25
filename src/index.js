87e9a2f4295160d9b3b4582b149e1f0b7aeff4b4
ric-cde <39746198+ric-cde@users.noreply.github.com>
2021-04-26 00:33:14 +0100

    hotfix

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ class App extends React.Component {
     <div class="container">
       <div class="child">
       <p>{process.env.PUBLIC_URL}</p>
-      <p><a href={presentation}>Part 1: Presentation</a><br /><br /></p>
-      <p><a href={report}>Part 2-4: Reports</a></p><br />
-      <iframe src={presentation} width="1000px" height="800px"></iframe>
+      <p><a href="https://sys-arch-ca3.s3-eu-west-1.amazonaws.com/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf">Part 1: Presentation</a><br /><br /></p>
+      <p><a href="https://sys-arch-ca3.s3-eu-west-1.amazonaws.com/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf">Part 2-4: Reports</a></p><br />
+      <iframe src="https://sys-arch-ca3.s3-eu-west-1.amazonaws.com/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf" width="1000px" height="800px"></iframe>
       <br /><br />
-      <iframe src={report} width="1000px" height="800px"></iframe>
+      <iframe src="https://sys-arch-ca3.s3-eu-west-1.amazonaws.com/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf" width="1000px" height="800px"></iframe>
       </div>
     </div>
     )
