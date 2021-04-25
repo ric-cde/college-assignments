51707837fb01eddaa77232059d1c72db329b7828
ric-cde <39746198+ric-cde@users.noreply.github.com>
2021-04-26 00:08:36 +0100

    hotfix

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ class App extends React.Component {
     return (
     <div class="container">
       <div class="child">
-      <p><a href="SYSARCHSEM220202021b-Cloud Computing Overview.pdf">Part 1: Presentation</a><br /><br /></p>
-      <p><a href="SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf">Part 2-4: Reports</a></p>
+      <p><a href={process.env.PUBLIC_URL + "/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf"}>Part 1: Presentation</a><br /><br /></p>
+      <p><a href={process.env.PUBLIC_URL + "/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf"}>Part 2-4: Reports</a></p><br />
+      <iframe src={process.env.PUBLIC_URL + "/SYSARCHSEM220202021b-Cloud_Computing_Overview.pdf"} width="1000px" height="800px"></iframe>
+      <br /><br />
+      <iframe src={process.env.PUBLIC_URL + "/SYSARCHASS220202021-RichardHerlihy_CA2-final.pdf"} width="1000px" height="800px"></iframe>
       </div>
     </div>
     )
